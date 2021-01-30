aefac8640f2155aa4524c12a1db8dbaa639f76aa
TJBachorz <TJBachorz@gmail.com>
2021-01-30 11:58:46 -0700

    changes DEBUG setting

--- abc_project/settings.py
+++ abc_project/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = ['abc-dl-backend.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['abc-dl-backend.herokuapp.com', '127.0.0.1',]
 
 # Application definition
 

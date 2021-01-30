2b0a4ea4e65727a8d49c57ecc67cf043e0b4093c
TJBachorz <TJBachorz@gmail.com>
2021-01-30 11:09:55 -0700

    deploys backend and adds heroku URL and localhost to ALLOWED_HOSTS

--- abc_project/settings.py
+++ abc_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ["https://abc-dl-backend.herokuapp.com/, '127.0.0.1']
 
 # Application definition
 

525bf8a3fb897376fe1f26cddce47fa226f903b2
TJBachorz <TJBachorz@gmail.com>
2021-01-30 11:01:39 -0700

    adds static roots

--- abc_project/settings.py
+++ abc_project/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTH_USER_MODEL = 'abc_app.Account'
 

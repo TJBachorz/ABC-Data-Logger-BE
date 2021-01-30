93daa444efccc37f9369ba763c02be45a93b9c97
TJBachorz <TJBachorz@gmail.com>
2021-01-30 11:27:32 -0700

    adds JWT dependency

--- abc_project/settings.py
+++ abc_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["abc-dl-backend.herokuapp.com", '127.0.0.1']
+ALLOWED_HOSTS = ['abc-dl-backend.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
